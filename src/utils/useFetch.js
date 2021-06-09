4337adc67a9010ff89a9ef7de64f22b9fdfb45ec
ynzrnk <71092723+ynzrnk@users.noreply.github.com>
2021-06-09 13:40:17 +0300

    Add clean up function to useEffect to stop fetching if the user leaves the page

--- src/utils/useFetch.js
+++ src/utils/useFetch.js
@@ -2,28 +2,33 @@ import { useState, useEffect } from 'react'
 
 function useFetch(url) {
   const [data, setData] = useState([])
-  const [isLoading, setIsLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(true)
   const [error, setError] = useState(null)
 
   // console.log('fetch data', 'data:', data)
 
   useEffect(() => {
-    async function fetchData() {
+    const stopFetch = new AbortController()
+    setTimeout(async () => {
       try {
-        setError(null) // no update was null
-        setIsLoading(true) // update was false
-        const response = await fetch(url)
+        const response = await fetch(url, { signal: stopFetch.signal })
         if (!response.ok) throw new Error('Could not fetch from this resourse')
         const data = await response.json()
-        setData(data) // update cause array always is a new reference to memory
-      } catch (err) {
-        setError(err.message)
-      } finally {
-        setIsLoading(false) // update was true
-      }
-    }
 
-    fetchData()
+        setData(data) // update cause array always is a new reference to memory
+        setIsLoading(false) // update was true
+        setError(null) // no update was null
+      } catch (err) {
+        if (err.name === 'AbortError') {
+          console.log('Fetch is stoped')
+        } else {
+          setError(err.message)
+          setIsLoading(false) // update was true
+        }
+      }
+    }, 1000)
+
+    return () => stopFetch.abort() //to stop request if component unmounts before it is finished
   }, [url]) // if state or props url will be updated useEffect will run again
   // it doesn't work if url is not a state (or props) variable
 
